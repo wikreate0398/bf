7761342d20a79ecc635aac66ac321430f2b32a65
Daniel <fleancu.daniel@gmail.com>
2019-02-20 20:29:31 +0200

    Добавил шаблон сайта

---
+++ public/website/dist/assets/scripts/main.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){document.body.style.zoom="90%",$("#header_not_registered").load("/partials/header_not_registered.html"),$("#header_registred").load("/partials/header_registered.html"),$("#footer").load("/partials/footer.html")});
+//# sourceMappingURL=main.js.map
---
+++ public/website/dist/assets/scripts/main.src.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	document.body.style.zoom = "90%" 
+
+	$('#header_not_registered').load('/partials/header_not_registered.html');
+	$('#header_registred').load('/partials/header_registered.html');
+	$('#footer').load('/partials/footer.html');
+
+})
