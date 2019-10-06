158bb1c0517f12c591bd350d187e46f9d767cbc2
Daniel <fleancu.daniel@gmail.com>
2019-10-06 14:04:57 +0300

    fix bugs

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="{{ url('personal_area/img/logo-black.png') }}" style="max-width: 150px;" alt="">
+            <img src="{{ url('img/logo.png') }}" style="max-width: 150px;" alt="">
         </a>
     </td>
 </tr>
